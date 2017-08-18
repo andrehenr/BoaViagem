d6b6552e60664613d66453f8ec1c1b44d2bf6862
andrehenr <andrehenriquetatu@gmail.com>
2017-08-17 23:29:25 -0300

    Initial commit

---
+++ app/src/main/java/br/com/casadocodigo/boaviagem/BoaviagemActivity.java
@@ -0,0 +1,41 @@
+package br.com.casadocodigo.boaviagem;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by ANDRE on 17/08/2017.
+ */
+
+public class BoaviagemActivity extends Activity {
+
+    private EditText usuario;
+    private EditText senha;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+        usuario = (EditText) findViewById(R.id.usuario);
+        senha = (EditText) findViewById(R.id.senha);
+    }
+    public void entrarOnClick(View v){
+
+        String usuarioInformado = usuario.getText().toString();
+        String senhaInformada = senha.getText().toString();
+
+        if("leitor".equals(usuarioInformado) &&
+                "123".equals(senhaInformada)){
+                
+        }
+        else{
+            String mensagemErro = getString(R.string.erro_autenticacao);
+            Toast toast = Toast.makeText(this, mensagemErro, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+}
